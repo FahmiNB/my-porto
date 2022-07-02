dfe108e482b65e2cf2c8b1a835f8785622bb8443
FahmiNB <fahmi123ganers@gmail.com>
2022-07-02 17:37:10 +0700

    change header

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render () {
         return (
             <div className='container-top-header'>
-                <h1 className='title-header'>CURRICULUM VITAE</h1>
+                <h1 className='title-header'>PORTOFOLIO</h1>
             </div>
         );
     }
